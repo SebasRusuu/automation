08057b3d69dc064cbad34909cc68ebbeaf5d2e0a
rususeba <sebastiao.rusu@mercedes-benz.com>
2024-07-25 14:27:15 +0100

    Maven Fair-part build

---
+++ java-selenium4/fair/src/main/java/com/mercedes_benz/qa/ui/package-info.java
@@ -0,0 +1,2 @@
+package com.mercedes_benz.qa.ui;
+//Care of poms and to do all the stuff about UI
---
+++ java-selenium4/fair/src/main/java/com/mercedes_benz/qa/ui/pom/components/ICookie.java
@@ -0,0 +1,6 @@
+package com.mercedes_benz.qa.ui.pom.components;
+
+public interface ICookie {
+    void acceptCookies() throws InterruptedException;
+
+}
