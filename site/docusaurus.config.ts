3e9bc08724cbe5bd810de3adf79df39d85f4cc60
rususeba <sebastiao.rusu@mercedes-benz.com>
2024-08-23 16:46:33 +0100

    Maven/QA-outstanding/github token and ssh

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://sebasrusuu.github.io/automation/',
+  url: 'https://sebasrusuu.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/automation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
